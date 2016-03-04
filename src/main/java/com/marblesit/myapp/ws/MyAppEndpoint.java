eb39866f1e3d4680f8c12c1dcb77b87e10c1010d
Cory Wandling <cory@marblesit.com>
2016-03-04 11:28:47 -0600

    Add db call to web service

--- src/main/java/com/marblesit/myapp/ws/MyAppEndpoint.java
+++ src/main/java/com/marblesit/myapp/ws/MyAppEndpoint.java
@@ -1,20 +1,29 @@
 package com.marblesit.myapp.ws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.marblesit.myapp.entity.Member;
+import com.marblesit.myapp.service.MyAppService;
 import com.marblesit.myapp.v1_0_0.GetMemberRequest;
 import com.marblesit.myapp.v1_0_0.GetMemberResponse;
 
 @Endpoint
 public class MyAppEndpoint {
+	
+	@Autowired
+	private MyAppService service;
 
 	@PayloadRoot(localPart="GetMemberRequest", namespace="http://marblesit.com/myapp/V1_0_0")
 	public @ResponsePayload GetMemberResponse getMember(@RequestPayload GetMemberRequest request) {
+		
+		Member member = service.getMember(request.getId());
+		
 		GetMemberResponse response = new GetMemberResponse();
-		response.setName("Cory");
+		response.setName(member.getName());
 		return response;
 	}
 
